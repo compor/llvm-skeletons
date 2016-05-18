3d5dc7f082888f56b3050f26316e90710aaa59d7
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2016-05-18 18:09:52 +0100

    fix include statement for consistency

--- skeleton-opt-minimal-pass/HelloPass.cpp
+++ skeleton-opt-minimal-pass/HelloPass.cpp
@@ -1,8 +1,8 @@
 
 #include "llvm/Pass.h"
-#include <llvm/IR/Type.h>
-#include <llvm/IR/Function.h>
-#include <llvm/Support/raw_ostream.h>
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
 
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
